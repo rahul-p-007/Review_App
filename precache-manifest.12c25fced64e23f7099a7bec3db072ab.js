c34ba424e29b891d5c9b29cc4a51c6d118f1b389
Rahulp_900 <rahulp00rtx@gmail.com>
2022-12-11 12:49:52 +0530

    Updates

---
+++ precache-manifest.12c25fced64e23f7099a7bec3db072ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9183ded7bbd3a736d7b1748a5d16ab",
+    "url": "/Review_App/index.html"
+  },
+  {
+    "revision": "9dc3a820aa2131654ab3",
+    "url": "/Review_App/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Review_App/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Review_App/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc3a820aa2131654ab3",
+    "url": "/Review_App/static/js/main.aa7d74ed.chunk.js"
+  },
+  {
+    "revision": "919bf72b1c555903ad28",
+    "url": "/Review_App/static/js/runtime-main.af805683.js"
+  }
+]);
